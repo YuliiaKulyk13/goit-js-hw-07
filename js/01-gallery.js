b94eb78287c0e10294177d422f4ea028f143a9b2
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-01-18 22:02:48 -0500

    created markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createMarkupGalleryItems(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createMarkupGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+console.log(galleryMarkup);
+
+const openModal = (event) => {
+  event.preventDefault();
+};
+
+// const instance = basicLightbox.create(` <img src="${}">`);
+
+// instance.show();
+
+galleryList.addEventListener("click", openModal);
